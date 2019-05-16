1cb727e71aef0d2e88c1ff0778867d85a1903da6
Vitor Isaac Sitton Maia <vitor2001bidi@gmail.com>
2019-05-16 15:25:32 -0300

    fixed URL's

--- src/app/services/get-todos.service.ts
+++ src/app/services/get-todos.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { toDo } from '../to-do/toDo';
 
+let URL = 'https://angular-todo-list-app.herokuapp.com';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,15 +13,15 @@ export class GetTodosService {
   constructor(private http: HttpClient) { }
 
   getTodos(): Observable<toDo[]>{
-    return this.http.get<toDo[]>('http://localhost:3000/getTodos');
+    return this.http.get<toDo[]>( URL + 'getTodos');
   }
 
   addTodo(todo: toDo): Observable<toDo>{
-    return this.http.post<toDo>('http://localhost:3000/addTodo', todo);
+    return this.http.post<toDo>(URL + '/addTodo', todo);
   }
 
   refreshTodos(todos: toDo[]){
-    return this.http.post<toDo[]>('http://localhost:3000/refresh', todos);
+    return this.http.post<toDo[]>(URL + '/refresh', todos);
   }
 
 }
